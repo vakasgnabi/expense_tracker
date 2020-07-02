b3a8fcf7d81044ad214bc73a9bfe569ba29fd6dc
vakasgnabi <vakasghulamnabi88@gmail.com>
2020-07-02 19:09:03 +0500

    Updated transactions list
    GlobalState removed because not used

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,5 +1,5 @@
 import React,{useContext} from 'react';
-import {GlobalContext, GlobalState} from '../context/GlobalState';
+import {GlobalContext} from '../context/GlobalState';
 
 
 export const TransactionList = () => {
